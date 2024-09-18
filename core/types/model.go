187de0ad62ec663077585871256d81cf81d97f6c
Flakerim Ismani <flakerimi@gmail.com>
2024-09-18 23:18:51 +0200

    Create model.go

---
+++ core/types/model.go
@@ -0,0 +1,13 @@
+package types
+
+type Pagination struct {
+	Total      int64 `json:"total"`
+	Page       int   `json:"page"`
+	PageSize   int   `json:"page_size"`
+	TotalPages int   `json:"total_pages"`
+}
+
+type PaginatedResponse struct {
+	Data       interface{} `json:"data"`
+	Pagination Pagination  `json:"pagination"`
+}
